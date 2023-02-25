d3ac9f9a1ac21b0f7eb233ef7309b77397864bdf
Yngvil <yngvilks@stud.ntnu.no>
2023-02-25 12:31:57 -0800

    corrected spelling in import

--- customdialogs.js
+++ customdialogs.js
@@ -1,10 +1,24 @@
 
 function showAlertDialog() {
     // Show the alert dialog
-    console.log('tries to show modal');
     document.getElementById('alertDialog').showModal();
+}
+
+function showConfirmDialog() {
+    // Show the confirm dialog
+    document.getElementById('confirmDialog').showModal();
+
+    // Check if user pressed cancel
+    let cancelClicked = false;
+    document.getElementById('cancelButton').addEventListener('click', function handleCancel() {
+        cancelClicked = true;
+    });
+    if (cancelClicked) {
+        document.getElementById('confirmDialog').close();
+    }
+    document.getElementById('output').textContent = `The value returned by confirm method is: ${!cancelClicked}`;
 }
 
 
 
-export { showAlertDialog }
+export { showAlertDialog, showConfirmDialog }
