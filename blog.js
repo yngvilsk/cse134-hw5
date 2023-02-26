570ef63ad0368e14be76eb94a1a68f1c4b974b4d
Yngvil <yngvilks@stud.ntnu.no>
2023-02-25 16:41:55 -0800

    add blog post button

--- blog.js
+++ blog.js
@@ -33,8 +33,16 @@ function showAddBlogPostDialog() {
 }
 
 let blogPosts = [
-    ['Homemade Acaii Bowl', 'February 13, 2023', 'Save money by making your favorouite bowl at home! My recipe has only 3 steps.'],
-    ['Top Movies 2022', 'December 22, 2022', 'Here is what I think were the best movies this year'],
+    {
+        'title': 'Homemade Acaii Bowl',
+        'date': '2023-02-03',
+        'summary': 'Save money by making your favorouite bowl at home! My recipe has only 3 steps.',
+    },
+    {
+        'title': 'Top Movies 2022',
+        'date': '2023-12-12',
+        'summary': 'Here is what I think were the best movies this year',
+    },
 ];
 
 function handleSaferPromptSubmit() {
@@ -58,7 +66,13 @@ function handleSaferPromptSubmit() {
         let cleanPostDate = DOMPurify.sanitize(dirtyPostDate);
         let cleanPostSummary = DOMPurify.sanitize(dirtyPostSummary);
 
-        blogPosts.push([cleanPostTitle, cleanPostDate, cleanPostSummary]);
+        blogPosts.push(
+            {
+                'title': cleanPostTitle,
+                'date': cleanPostDate,
+                'summary': cleanPostSummary,
+            }
+        );
 
         updateBlogPostList();
     }
